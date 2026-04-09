416f809f2582237555eb4722c37b1ab85e7ce177
X9DestroX <150329572+X9DestroX@users.noreply.github.com>
2026-04-09 15:33:25 +0530

    Update firebase-config.js

--- db/firebase-config.js
+++ db/firebase-config.js
@@ -1,21 +1,22 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"; 
-import { getAuth } from "firebase/auth";
+// Imports
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-//replace with workingkey
+// 2nd acc Firebase configuration
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
+  apiKey: "AIzaSyDINqAKeeoVU8KRhvYc_c9PMkYCkR07Z1I",
+  authDomain: "bniattendance-96205.firebaseapp.com",
+  projectId: "bniattendance-96205",
+  storageBucket: "bniattendance-96205.firebasestorage.app",
+  messagingSenderId: "250805340036",
+  appId: "1:250805340036:web:0eaf64fcecc66535a27fd1",
+  measurementId: "G-NY8CV4X36R"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Exports allow other modules (auth.js, app.js) to access the DB and Auth
+// Export instances to use in auth.js and app.js
 export const db = getFirestore(app);
 export const auth = getAuth(app);
