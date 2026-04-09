2847bb1cfa6f56778c35dc56a5aa2d45aa10af2a
X9DestroX <150329572+X9DestroX@users.noreply.github.com>
2026-04-09 16:49:51 +0530

    Refactor Firebase configuration and exports

--- db/firebase-config.js
+++ db/firebase-config.js
@@ -1,22 +1,17 @@
-// Firebase imports
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDINqAKeeoVU8KRhvYc_c9PMkYCkR07Z1I",
   authDomain: "bniattendance-96205.firebaseapp.com",
   projectId: "bniattendance-96205",
   storageBucket: "bniattendance-96205.appspot.com",
   messagingSenderId: "250805340036",
-  appId: "1:250805340036:web:0eaf64fcecc66535a27fd1",
-  measurementId: "G-NY8CV4X36R"
+  appId: "1:250805340036:web:0eaf64fcecc66535a27fd1"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Export services
 export const db = getFirestore(app);
 export const auth = getAuth(app);
